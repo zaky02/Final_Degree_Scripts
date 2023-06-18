8e90f3de3a3f96a768522d0f36ff98ba8d3bbf17
Isaac Soul <106395931+zaky02@users.noreply.github.com>
2023-06-18 21:12:53 +0200

    Update filter_ligands.py

--- FDG_scripts/PELE/filter_ligands.py
+++ FDG_scripts/PELE/filter_ligands.py
@@ -12,11 +12,11 @@ x_csv = pd.read_csv("/gpfs/projects/bsc72/bsc72876/PELE_KRAS_2/KRAS_x.csv")
 threshold = float(x_csv["all_BFE"])
 print(threshold) #-90.8071
 
-s4_csv_filtered = s4_csv.loc[s4_csv["all_BFE"] < -90.8071]
+s4_csv_filtered = s4_csv.loc[s4_csv["all_BFE"] < -104.347]
 s4_csv_filtered.to_csv('/gpfs/projects/bsc72/bsc72876/PELE_KRAS_2/KRAS_s4_filtered.csv')
 
-s3_csv_filtered = s3_csv.loc[s3_csv["all_BFE"] < -90.8071]
+s3_csv_filtered = s3_csv.loc[s3_csv["all_BFE"] < -104.347]
 s3_csv_filtered.to_csv('/gpfs/projects/bsc72/bsc72876/PELE_KRAS_2/KRAS_s3_filtered.csv')
 
-s2_csv_filtered = s2_csv.loc[s2_csv["all_BFE"] < -90.8071]
+s2_csv_filtered = s2_csv.loc[s2_csv["all_BFE"] < -104.347]
 s2_csv_filtered.to_csv('/gpfs/projects/bsc72/bsc72876/PELE_KRAS_2/KRAS_s2_filtered.csv')
